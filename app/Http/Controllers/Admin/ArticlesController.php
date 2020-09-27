69a570e34fbd655befd2f4c44675f3d95dae81cd
Kirill_Peretiatko <Kyr.nk.go@gmail.com>
2020-09-27 13:56:50 +0300

    route and controller Articles

---
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function addArticle()
+    {
+
+    }
+
+    public function editArticle(int $id)
+    {
+
+    }
+
+    public function deleteArticle(Request $request)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,23 @@ Route::group(['middleware' => 'auth'], function () {
     Route::group(['middleware' => 'admin', 'prefix' => 'admin'], function () {
         Route::get('/', 'Admin\AccountController@index')->name('admin');
 
+        /** Categories */
         Route::get('/categories', 'Admin\CategoriesController@index')->name('categories');
-
         Route::get('/categories/add', 'Admin\CategoriesController@addCategory')->name('categories.add');
         Route::post('/categories/add', 'Admin\CategoriesController@addRequestCategory');
-
         Route::get('/categories/edit/{id}', 'Admin\CategoriesController@editCategory')
             ->where('id', '\d+')
             ->name('categories.edit');
         Route::post('/categories/edit/{id}', 'Admin\CategoriesController@editRequestCategory')
             ->where('id', '\d+');
-
         Route::get('/categories/delete', 'Admin\CategoriesController@deleteCategory')->name('categories.delete');
+
+        /** Articles */
+        Route::get('/articles', 'Admin\ArticlesController@index')->name('articles');
+        Route::get('/articles/add', 'Admin\ArticlesController@addArticle')->name('articles.add');
+        Route::get('/articles/edit/{id}', 'Admin\ArticlesController@editArticle')
+            ->where('id', '\d+')
+            ->name('articles.edit');
+        Route::get('/articles/delete', 'Admin\ArticlesController@deleteArticle')->name('articles.delete');
     });
 });
